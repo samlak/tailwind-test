188c221e2a057ff934a0cfbb37700e4cf8458c75
Salami Haruna <samlak1999@gmail.com>
2022-01-04 10:14:29 +0100

    search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -15,7 +15,7 @@ function Search ({setImageData}) {
 
   return (
     <div className="flex justify-center mt-24">
-      <input onChange={inputUpdate} className="w-1/2 px-7 h-14 border border-blue-400 rounded-tl-3xl rounded-bl-3xl mb-12" type="search" name="search" placeholder="Search image" />
+      <input onChange={inputUpdate} className="sm:w-1/2 w-3/4  px-7 h-14 border border-blue-400 rounded-tl-3xl rounded-bl-3xl mb-12" type="search" name="search" placeholder="Search image" />
       <button onClick={searchImage} className="h-14 border border-blue-400 hover:border-blue-600 bg-blue-400 hover:bg-blue-600 mb-12 rounded-tr-3xl rounded-br-3xl text-white px-6 py-4">
         <img src={searchIcon} alt="Search Icon" />
       </button>
