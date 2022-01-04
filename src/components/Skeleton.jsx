86ecf6565da5409d57153b001f6613228b81cc24
Salami Haruna <samlak1999@gmail.com>
2022-01-04 10:16:17 +0100

    skeleton update

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,6 +1,6 @@
 function Skeleton () {
   return (
-    <div className="lg:w-1/4 md:w-1/3 w-1/2 p-5 h-72">
+    <div className="xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-full w-full p-5 md:h-72 sm:h-96 h-96">
       <div className="border border-blue-100 shadow-md rounded-md p-0 w-full h-full">
         <div className="animate-pulse w-full h-full">
           <div className="w-full h-full bg-blue-100 rounded"></div>
